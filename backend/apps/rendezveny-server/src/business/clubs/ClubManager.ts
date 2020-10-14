c3a0035975dfd8d3b42444c0ab5185a3130be08c
Mihály Dobos-Kovács <dkmisu@gmail.com>
2020-10-14 23:30:08 +0200

    Refactor club module

--- backend/apps/rendezveny-server/src/business/clubs/ClubManager.ts
+++ backend/apps/rendezveny-server/src/business/clubs/ClubManager.ts
@@ -2,39 +2,54 @@ import { Injectable } from '@nestjs/common';
 import { EntityManager, Repository, Transaction, TransactionManager } from 'typeorm';
 import { Club } from '../../data/models/Club';
 import { checkArgument } from '../../utils/preconditions';
-import { InvalidPaginationPageSizeException } from '../utils/InvalidPaginationPageSizeException';
-import { InvalidPaginationOffsetException } from '../utils/InvalidPaginationOffsetException';
 import { ClubWithNameExistsException } from './ClubWithNameExistsException';
 import { ClubDoesNotExistsException } from './ClubDoesNotExistsException';
+import { ClubMembership } from '../../data/models/ClubMembership';
 import { InjectRepository } from '@nestjs/typeorm';
+import { nameof } from '../../utils/nameof';
+import { checkPagination } from '../utils/CheckPagination';
+import { isNotEmpty } from 'class-validator';
+import { ClubNameValidationException } from './ClubNameValidationException';
+import { AccessToken } from '../auth/AuthTokens';
+import { checkPermission } from '../utils/CheckPermissions';
+import { UserRole } from '../../data/models/UserRole';
+import { ClubRole } from '../../data/models/ClubRole';
 
 @Injectable()
 export class ClubManager {
 	public constructor(
-		@InjectRepository(Club) private readonly clubRepository: Repository<Club>
+		@InjectRepository(Club) private readonly clubRepository: Repository<Club>,
+		@InjectRepository(ClubMembership) private readonly membershipRepository: Repository<ClubMembership>
 	) {}
 
-	public async getAllClubs(): Promise<{ clubs: Club[], count: number}> {
+	public async getAllClubs(
+		accessContext: AccessToken
+	): Promise<{ clubs: Club[], count: number}> {
+		checkPermission(accessContext.rol === UserRole.USER, accessContext.rol === UserRole.ADMIN);
+
 		const [clubs, count] = await this.clubRepository.findAndCount();
 		return { clubs, count };
 	}
 
 	public async getAllClubsPaginated(
-		pageSize: number, offset: number
+		accessContext: AccessToken, pageSize: number, offset: number
 	): Promise<{ clubs: Club[], count: number}> {
-		checkArgument(pageSize > 0, InvalidPaginationPageSizeException, pageSize);
-		checkArgument(offset >= 0, InvalidPaginationOffsetException, offset);
+		checkPagination(pageSize, offset);
+		checkPermission(accessContext.rol === UserRole.USER, accessContext.rol === UserRole.ADMIN);
 
 		const [clubs, count] = await this.clubRepository.findAndCount({
 			take: pageSize,
 			skip: offset * pageSize
 		});
+
 		return { clubs, count };
 	}
 
 	public async getClub(
-		id: string
+		accessContext: AccessToken, id: string
 	): Promise<Club> {
+		checkPermission(accessContext.rol === UserRole.USER, accessContext.rol === UserRole.ADMIN);
+
 		const club = await this.clubRepository.findOne({ id });
 
 		if(!club) {
@@ -47,26 +62,31 @@ export class ClubManager {
 
 	@Transaction()
 	public async addClub(
-		name: string,
+		accessContext: AccessToken, name: string,
 		@TransactionManager() _entityManager?: EntityManager
 	): Promise<Club> {
+		checkArgument(isNotEmpty(name), ClubNameValidationException);
+		checkPermission(accessContext.rol === UserRole.ADMIN);
+
 		const clubWithName = await this.clubRepository.findOne({ name });
 
 		if(clubWithName) {
 			throw new ClubWithNameExistsException(name);
 		}
 		else {
-			const club = new Club();
-			club.name = name;
+			const club = new Club({ name });
 			return this.clubRepository.save(club);
 		}
 	}
 
 	@Transaction()
 	public async editClub(
-		id: string, name: string,
+		accessContext: AccessToken, id: string, name: string,
 		@TransactionManager() _entityManager?: EntityManager
 	): Promise<Club> {
+		checkArgument(isNotEmpty(name), ClubNameValidationException);
+		checkPermission(accessContext.rol === UserRole.ADMIN);
+
 		const club = await this.clubRepository.findOne({ id });
 
 		if(!club) {
@@ -80,9 +100,11 @@ export class ClubManager {
 
 	@Transaction()
 	public async deleteClub(
-		id: string,
+		accessContext: AccessToken, id: string,
 		@TransactionManager() _entityManager?: EntityManager
 	): Promise<void> {
+		checkPermission(accessContext.rol === UserRole.ADMIN);
+
 		const club = await this.clubRepository.findOne({ id });
 
 		if(!club) {
@@ -90,6 +112,64 @@ export class ClubManager {
 		}
 		else {
 			await this.clubRepository.remove(club);
+		}
+	}
+
+	public async getAllClubMemberships(
+		accessContext: AccessToken, id: string
+	): Promise<{ memberships: ClubMembership[], count: number}> {
+		checkPermission(
+			(
+				accessContext.rol === UserRole.USER
+				&& accessContext.clb.filter(clb => clb.cid === id && clb.rol === ClubRole.CLUB_MANAGER).length > 0
+			),
+			accessContext.rol === UserRole.ADMIN
+		);
+
+		const club = await this.clubRepository.findOne({ id }, {
+			relations: [
+				nameof<Club>('memberships')
+			]
+		});
+
+		if(!club) {
+			throw new ClubDoesNotExistsException(id);
+		}
+		else {
+			return {
+				memberships: club.memberships,
+				count: club.memberships.length
+			};
+		}
+	}
+
+	public async getAllClubMembershipsPaginated(
+		accessContext: AccessToken, id: string, pageSize: number, offset: number
+	): Promise<{ memberships: ClubMembership[], count: number}> {
+		checkPagination(pageSize, offset);
+		checkPermission(
+			(
+				accessContext.rol === UserRole.USER
+				&& accessContext.clb.filter(clb => clb.cid === id && clb.rol === ClubRole.CLUB_MANAGER).length > 0
+			),
+			accessContext.rol === UserRole.ADMIN
+		);
+
+		const club = await this.clubRepository.findOne({ id });
+
+		if(!club) {
+			throw new ClubDoesNotExistsException(id);
+		}
+		else {
+			const [memberships, count] = await this.membershipRepository.findAndCount({
+				where: { club },
+				take: pageSize,
+				skip: offset * pageSize
+			});
+			return {
+				memberships,
+				count
+			};
 		}
 	}
 }
