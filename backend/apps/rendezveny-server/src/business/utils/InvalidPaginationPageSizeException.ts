7daa3f2acbc5e148d56b4c85a9a5b10d30237ccc
Mihály Dobos-Kovács <dkmisu@gmail.com>
2020-10-14 23:27:14 +0200

    Refactor pagination

--- backend/apps/rendezveny-server/src/business/utils/InvalidPaginationPageSizeException.ts
+++ backend/apps/rendezveny-server/src/business/utils/InvalidPaginationPageSizeException.ts
@@ -4,7 +4,7 @@ export class InvalidPaginationPageSizeException extends BusinessException {
 	public constructor(pageSize: number) {
 		super(
 			'INVALID_PAGINATION_PAGE_SIZE',
-			`Invalid pagination page size: expected > 0, got: ${pageSize}`,
+			`Invalid pagination page size: 0 < expected <= 100, got: ${pageSize}`,
 			{ pageSize }
 		);
 	}
