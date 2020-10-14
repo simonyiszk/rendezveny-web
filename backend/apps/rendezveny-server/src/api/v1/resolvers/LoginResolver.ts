48e7276b672d02527c8a868a1c2f4df87b97533d
Mihály Dobos-Kovács <dkmisu@gmail.com>
2020-10-14 23:31:28 +0200

    Add login resolver

---
+++ backend/apps/rendezveny-server/src/api/v1/resolvers/LoginResolver.ts
@@ -0,0 +1,52 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { GraphQLBoolean, GraphQLString } from 'graphql';
+import { AuthManager } from '../../../business/auth/AuthManager';
+import { UseFilters, UseGuards } from '@nestjs/common';
+import { BusinessExceptionFilter } from '../utils/BusinessExceptionFilter';
+import { AuthRefreshGuard, RefreshContext } from '../../../business/auth/AuthRefreshJwtStrategy';
+import { RefreshToken } from '../../../business/auth/AuthTokens';
+import { LoginDTO } from '../dtos/LoginDTO';
+
+@Resolver()
+export class LoginResolver {
+	public constructor(
+		private readonly authManager: AuthManager
+	) {}
+
+	@Mutation(_ => LoginDTO, {
+		name: 'login_withLocalIdentity',
+		description: 'Logs the user in with username and password'
+	})
+	@UseFilters(BusinessExceptionFilter)
+	public async loginWithLocalIdentity(
+		@Args('username', { description: 'The username of the user' }) username: string,
+		@Args('password', { description: 'The password of the user' }) password: string
+	): Promise<LoginDTO> {
+		return this.authManager.loginWithLocalIdentity(username, password);
+	}
+
+	@Mutation(_ => GraphQLString, {
+		name: 'login_withRefreshToken',
+		description: 'Logs the user in with a refresh token'
+	})
+	@UseFilters(BusinessExceptionFilter)
+	@UseGuards(AuthRefreshGuard)
+	public async loginWithRefreshToken(
+		@RefreshContext() refreshContext: RefreshToken
+	): Promise<string> {
+		return this.authManager.loginWithRefreshToken(refreshContext);
+	}
+
+	@Mutation(_ => GraphQLBoolean, {
+		name: 'login_logout',
+		description: 'Logs the user out'
+	})
+	@UseFilters(BusinessExceptionFilter)
+	@UseGuards(AuthRefreshGuard)
+	public async logout(
+		@RefreshContext() refreshContext: RefreshToken
+	): Promise<boolean> {
+		await this.authManager.logout(refreshContext);
+		return true;
+	}
+}
