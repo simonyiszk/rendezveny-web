5f4a2c41efd5892940129de3cae96700101afcca
Freyb <balazs.frey@gmail.com>
2020-10-31 00:31:44 +0100

    Add Nav buttons

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,14 @@
-import { Box, Image, Link as ChakraLink } from '@chakra-ui/core';
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Link as ChakraLink,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/core';
 import { Link } from 'gatsby';
 import React from 'react';
 
@@ -7,22 +17,45 @@ import logo from '../assets/images/simonyi_white_white.svg';
 
 export default function Header(): JSX.Element {
   return (
-    <Box p={1} backgroundColor="simonyi">
-      <Link to="/">
-        <Image
-          display={['none', null, 'block']}
-          height="2rem"
-          src={logo}
-          alt="Logo"
-        />
-        <Image
-          display={['block', null, 'none']}
-          height="3rem"
-          m={1}
-          src={logoSmall}
-          alt="Logo"
-        />
-      </Link>
-    </Box>
+    <Flex p={1} backgroundColor="simonyi">
+      <Box>
+        <Link to="/">
+          <Image
+            display={['none', null, 'block']}
+            height="2rem"
+            src={logo}
+            alt="Logo"
+          />
+          <Image
+            display={['block', null, 'none']}
+            height="3rem"
+            m={1}
+            src={logoSmall}
+            alt="Logo"
+          />
+        </Link>
+      </Box>
+      <Flex flexGrow={1} justifyContent="flex-end">
+        <Flex mr="1rem">
+          <Menu>
+            <MenuButton>Rendezvények</MenuButton>
+            <MenuList>
+              <MenuItem>Rendezvények</MenuItem>
+              <MenuItem>Történet</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+        <Flex mr="1rem">
+          <Menu>
+            <MenuButton>Profil</MenuButton>
+            <MenuList>
+              <MenuItem>Profil</MenuItem>
+              <MenuItem>Logok</MenuItem>
+              <MenuItem>Kilépés</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
