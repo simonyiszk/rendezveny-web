2a08462630da20b145fa5cc594855b9b5718ff2e
Freyb <balazs.frey@gmail.com>
2020-10-31 00:05:38 +0100

    Fill startDate mock data

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -12,9 +12,14 @@ export default function IndexPage(): JSX.Element {
   const [otherEvents, setOtherEvents] = useState<Event[]>([]);
 
   useEffect(() => {
-    setOrganizedEvents([{ id: 1, name: 'Test event 1' }]);
-    setAppliedEvents([{ id: 2, name: 'Test event 2' }]);
-    setOtherEvents([{ id: 3, name: 'Test event 3' }]);
+    setOrganizedEvents([
+      { id: 1, name: 'Test event 1', startDate: '2020-11-10' },
+      { id: 4, name: 'Test event 4', startDate: '2020-11-10' },
+    ]);
+    setAppliedEvents([
+      { id: 2, name: 'Test event 2', startDate: '2020-11-10' },
+    ]);
+    setOtherEvents([{ id: 3, name: 'Test event 3', startDate: '2020-11-10' }]);
   }, []);
 
   return (
