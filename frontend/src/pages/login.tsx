2d7012c8e3f4f79de8b20e5f00c7e74c96238604
Freyb <balazs.frey@gmail.com>
2020-10-31 01:09:54 +0100

    Create Login page

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from '../components/Layout';
+
+export default function LoginPage(): JSX.Element {
+  return (
+    <Layout>
+      <p>Please login</p>
+    </Layout>
+  );
+}
