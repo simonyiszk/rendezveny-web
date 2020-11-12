02bf7456ea4fdcf37c2915350a17e4dc96e10c35
Freyb <balazs.frey@gmail.com>
2020-11-12 16:16:11 +0100

    Install gatsby-source-graphql

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -10,6 +10,17 @@ module.exports = {
     'gatsby-plugin-chakra-ui',
     'gatsby-plugin-emotion',
     {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `ApiV1Module`,
+        fieldName: `apiv1`,
+        url: `http://localhost:3000/api/v1`,
+        headers: {
+          Authorization: `Bearer x`,
+        },
+      },
+    },
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         ...siteMetadata,
