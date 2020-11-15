7e4ae7ad6676604f02b761f6321d3ad0cb11f513
Freyb <balazs.frey@gmail.com>
2020-11-15 14:54:53 +0100

    Fix path issue in gatsby-browser.js

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -1,7 +1,7 @@
 import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 
-import useAppApolloClient from './src/utils/ApolloClient';
+import useAppApolloClient from './src/utils/token/ApolloClient';
 
 // eslint-disable-next-line import/prefer-default-export
 export const wrapRootElement = ({ element }) => (
