3df8b39ebe6de48eaea97371a3c6fd1c05f958b2
Freyb <balazs.frey@gmail.com>
2020-11-15 15:54:17 +0100

    Add link 'to' prop to Button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,17 +1,34 @@
 import { BoxProps, Flex } from '@chakra-ui/core';
+import { Link } from 'gatsby';
 import React from 'react';
 
 interface Props extends BoxProps {
-  onClick: () => void;
+  onClick?: () => void;
+  to?: string;
   text: string;
 }
 
+const wrapComponent = (
+  children: JSX.Element,
+  to: string | undefined,
+): JSX.Element => {
+  if (to) {
+    return (
+      <Flex>
+        <Link to={to}>{children}</Link>
+      </Flex>
+    );
+  }
+  return <>{children}</>;
+};
+
 export default function Button({
   onClick,
+  to,
   text,
   ...props
 }: Props): JSX.Element {
-  return (
+  return wrapComponent(
     <Flex
       px="1rem"
       py="0.5rem"
@@ -23,6 +40,7 @@ export default function Button({
       {...props}
     >
       {text}
-    </Flex>
+    </Flex>,
+    to,
   );
 }
