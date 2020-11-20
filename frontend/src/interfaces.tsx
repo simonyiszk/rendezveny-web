deb9cd3faa81b7158cbc3fb7d1a8f89fdf162500
Freyb <balazs.frey@gmail.com>
2020-11-20 15:19:19 +0100

    Update interfaces

--- frontend/src/interfaces.tsx
+++ frontend/src/interfaces.tsx
@@ -1,10 +1,24 @@
 export interface Event {
   id: number;
-  name: string; // TODO
-  startDate: string;
+  name: string;
+  description: string;
+  place: string;
+  start: string;
+  end: string;
+  registrationStart: string;
+  registrationEnd: string;
+  isClosedEvent: boolean;
 }
 
 export interface HistoryYear {
   year: number;
   events: Event[];
+}
+
+export interface FormQuestion {
+  question: string;
+  isRequired: boolean;
+  type: string;
+  order: number;
+  possibleAnswers?: string[];
 }
