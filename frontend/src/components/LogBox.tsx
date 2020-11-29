2c121a44b8580d8e5b3db856921e49458551de72
Freyb <balazs.frey@gmail.com>
2020-11-29 18:25:26 +0100

    Style Profile and Logs pages

--- frontend/src/components/LogBox.tsx
+++ frontend/src/components/LogBox.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, Flex } from '@chakra-ui/core';
+import { Box, BoxProps, Flex, Grid } from '@chakra-ui/core';
 import React from 'react';
 
 import { Log } from '../interfaces';
@@ -9,18 +9,35 @@ interface Props extends BoxProps {
 
 export default function LogBox({ log }: Props): JSX.Element {
   return (
-    <Flex my="0.25rem" flexDir="column">
-      <Flex>
+    <Flex mb="1rem" justifyContent="center">
+      <Grid
+        width={['80%', null, null, '100%']}
+        gridTemplateColumns={['1fr', null, null, '1fr 1fr']}
+        columnGap={['0', null, '1rem']}
+      >
         <Box fontWeight="bold">
           {new Date(log.at).toLocaleDateString()}{' '}
           {new Date(log.at).toLocaleTimeString()}
         </Box>
-        <Box>{log.type}</Box>
-      </Flex>
-      <Flex>
-        <Box>{log.query}</Box>
-        <Box>{log.result}</Box>
-      </Flex>
+        <Flex>
+          <Box fontWeight="bold" mr={1}>
+            {log.type}
+          </Box>
+          <Box>({log.issuerId})</Box>
+        </Flex>
+        <Flex>
+          <Box fontWeight="bold" mr={1}>
+            Query:
+          </Box>
+          <Box>{log.query}</Box>
+        </Flex>
+        <Flex>
+          <Box fontWeight="bold" mr={1}>
+            Result:
+          </Box>
+          <Box>{log.result}</Box>
+        </Flex>
+      </Grid>
     </Flex>
   );
 }
--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -9,7 +9,7 @@ import { Layout } from '../components/Layout';
 import LinkButton from '../components/LinkButton';
 import LogBox from '../components/LogBox';
 import SectionHeader from '../components/SectionHeader';
-import { User } from '../interfaces';
+import { ClubRole, User } from '../interfaces';
 import { useProfileGetSelfQuery } from '../utils/api/profile/UserGetSelfQuery';
 import ProtectedComponent from '../utils/protection/ProtectedComponent';
 
@@ -20,14 +20,19 @@ export default function LogsPage(): JSX.Element {
     navigate('/');
     return <div>Error</div>;
   }
-
   return (
     <Layout>
-      <Box>{getEvents.data?.users_getSelf.name}</Box>
-      <Box>Körök</Box>
+      <Box fontSize="2rem" fontWeight="bold">
+        {getEvents.data?.users_getSelf.name}
+      </Box>
+      <Box mt={4}>Körök</Box>
       {getEvents.data?.users_getSelf.clubMemberships.nodes.map((c) => (
         <Box key={c.club.id}>
-          {c.club.name} {c.role}
+          {c.club.name} (
+          {c.role.toString() === ClubRole[ClubRole.MEMBER]
+            ? 'Tag'
+            : 'Körvezető'}
+          )
         </Box>
       ))}
     </Layout>
