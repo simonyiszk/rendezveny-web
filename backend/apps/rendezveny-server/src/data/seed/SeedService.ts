689f31929d9b5cff987dfdeeff692ab733afd0fd
Freyb <balazs.frey@gmail.com>
2021-06-06 17:59:35 +0200

    Add new initial data

--- backend/apps/rendezveny-server/src/data/seed/SeedService.ts
+++ backend/apps/rendezveny-server/src/data/seed/SeedService.ts
@@ -13,12 +13,9 @@ import { Event } from '../models/Event';
 import { Registration } from '../models/Registration';
 import { TemporaryIdentity } from '../models/TemporaryIdentity';
 import { Organizer } from '../models/Organizer';
-import { OrganizerNotificationSettings } from '../models/OrganizerNotificationSettings';
-import { RegistrationNotificationSettings } from '../models/RegistrationNotificationSettings';
 import { AuthManager } from '../../business/auth/AuthManager';
 import { EventManager } from '../../business/events/EventManager';
 import { JwtService } from '@nestjs/jwt';
-import { AccessContext, AccessToken } from '../../business/auth/tokens/AccessToken';
 import { FormQuestion, FormQuestionType } from '../models/FormQuestion';
 import { FormQuestionAnswer } from '../models/FormQuestionAnswer';
 import { HRTable } from '../models/HRTable';
@@ -27,6 +24,7 @@ import { HRSegment } from '../models/HRSegment';
 import { Log } from '../models/Log';
 import { AuthSCHIdentity } from '../models/AuthSCHIdentity';
 import { FormQuestionTemplate } from '../models/FormQuestionTemplate';
+import { CryptoService } from '../../business/crypto/CryptoService';
 
 @Injectable()
 export class SeedService {
@@ -51,7 +49,8 @@ export class SeedService {
 		@InjectRepository(Log) private readonly logRepository: Repository<Log>,
 		private readonly authManager: AuthManager,
 		private readonly eventManager: EventManager,
-		private readonly jwtService: JwtService
+		private readonly jwtService: JwtService,
+		private readonly cryptoService: CryptoService
 	) {}
 
 	public async clearDatabase(): Promise<void> {
@@ -79,395 +78,131 @@ export class SeedService {
 		}
 	}
 
+	private readonly salt =
+		process.env.PASSWORD_SALT ??
+		'G5wEYCwf2rFtgZq41j+yDYXAvgoGmKjwNWAM+wgG0f8iG3r3xRx9G+Inup5Gd0J521XMHXLQVIL/MJQa0YcOzw==';
+
+	private readonly members = {
+		cucu: 'bozsik.szabolcs',
+		dani: 'vanko.daniel',
+		dave: 'kiss.david',
+		bea: 'sudi.beata',
+		bazs: 'frey.balazs',
+		balazska: 'bidlo.balazs',
+		adi: 'kiss.adam',
+		johnny: 'feher.janos',
+		barni: 'borcsok.barnabas',
+		laci: 'sepsi.laszlo',
+		andris: 'borbas.andras',
+		pali: 'weisz.pal'
+	};
+
+	private readonly groups = [
+		'ac',
+		'bss',
+		'mgmt',
+		'kirdev',
+		'sem',
+		'schdesign',
+		'phd',
+		'cica',
+		'kezek',
+		'simonyi',
+		'oregek',
+		'weeb'
+	];
+
+	private readonly memberships = {
+		ac: { lead: 'cucu', members: ['dave', 'balazska', 'andris', 'pali'] },
+		bss: { lead: 'balazska', members: ['cucu', 'dave', 'barni', 'pali'] },
+		mgmt: { lead: 'dani', members: ['cucu', 'dave', 'bea', 'bazs'] },
+		kirdev: { lead: 'laci', members: ['dani', 'bazs', 'johnny', 'barni'] },
+		sem: { lead: 'pali', members: ['bea', 'adi', 'balazska', 'andris'] },
+		schdesign: { lead: 'barni', members: ['dani', 'bazs', 'johnny', 'andris'] },
+		phd: { lead: 'adi', members: ['bea', 'balazska', 'laci', 'pali'] },
+		cica: { lead: 'bea', members: ['cucu', 'adi', 'laci', 'andris'] },
+		kezek: { lead: 'dave', members: ['bea', 'johnny', 'barni', 'pali'] },
+		simonyi: { lead: 'bazs', members: ['cucu', 'dani', 'balazska', 'barni'] },
+		oregek: { lead: 'johnny', members: ['dave', 'bazs', 'adi', 'laci'] },
+		weeb: { lead: 'andris', members: ['dani', 'adi', 'johnny', 'laci'] }
+	};
+
 	public async seedDatabase(): Promise<void> {
 		/* Users */
 
-		const john = new User({ name: 'John' });
-		await this.userRepository.save(john);
-		await this.localIdentityRepository.save(
-			new LocalIdentity({
-				username: 'john',
-				email: 'john@localhost',
-				password: 'eQ8Q6H1OKUFMlxspJlM2FYTa6JlifufuqdEM+ULLRXzhalHO6PZN1nRQoEh/7zwTDJ+gvXaDeln1OGDTjG/1KQ==', // "admin"
-				passwordVersion: 1,
-				salt: 'G5wEYCwf2rFtgZq41j+yDYXAvgoGmKjwNWAM+wgG0f8iG3r3xRx9G+Inup5Gd0J521XMHXLQVIL/MJQa0YcOzw==',
-				user: john
-			})
-		);
+		const membersCreated = {} as { [key: string]: User };
 
-		const peter = new User({ name: 'Peter' });
-		await this.userRepository.save(peter);
+		for (const [k, v] of Object.entries(this.members)) {
+			const obj = new User({ name: k });
+			await this.userRepository.save(obj);
+			await this.localIdentityRepository.save(
+				new LocalIdentity({
+					username: v,
+					email: `${k}@localhost`,
+					password: (await this.cryptoService.hashPassword('x', 1, this.salt)).hashedPassword,
+					passwordVersion: 1,
+					salt: this.salt,
+					user: obj
+				})
+			);
+			membersCreated[k] = obj;
+		}
 
-		const april = new User({ name: 'April' });
-		await this.userRepository.save(april);
-		await this.localIdentityRepository.save(
-			new LocalIdentity({
-				username: 'april',
-				email: 'april@localhost',
-				password: 'eQ8Q6H1OKUFMlxspJlM2FYTa6JlifufuqdEM+ULLRXzhalHO6PZN1nRQoEh/7zwTDJ+gvXaDeln1OGDTjG/1KQ==', // "admin"
-				passwordVersion: 1,
-				salt: 'G5wEYCwf2rFtgZq41j+yDYXAvgoGmKjwNWAM+wgG0f8iG3r3xRx9G+Inup5Gd0J521XMHXLQVIL/MJQa0YcOzw==',
-				user: april
-			})
-		);
-
-		const emily = new User({ name: 'Emily' });
-		await this.userRepository.save(emily);
-		await this.localIdentityRepository.save(
-			new LocalIdentity({
-				username: 'emily',
-				email: 'emily@localhost',
-				password: 'eQ8Q6H1OKUFMlxspJlM2FYTa6JlifufuqdEM+ULLRXzhalHO6PZN1nRQoEh/7zwTDJ+gvXaDeln1OGDTjG/1KQ==', // "admin"
-				passwordVersion: 1,
-				salt: 'G5wEYCwf2rFtgZq41j+yDYXAvgoGmKjwNWAM+wgG0f8iG3r3xRx9G+Inup5Gd0J521XMHXLQVIL/MJQa0YcOzw==',
-				user: emily
-			})
-		);
-
-		const admin = new User({ name: 'Administrator', role: UserRole.ADMIN });
+		const admin = new User({ name: 'admin', role: UserRole.ADMIN });
 		await this.userRepository.save(admin);
 		await this.localIdentityRepository.save(
 			new LocalIdentity({
 				username: 'admin',
 				email: 'admin@localhost',
-				password: 'eQ8Q6H1OKUFMlxspJlM2FYTa6JlifufuqdEM+ULLRXzhalHO6PZN1nRQoEh/7zwTDJ+gvXaDeln1OGDTjG/1KQ==', // "admin"
+				password: (await this.cryptoService.hashPassword('admin', 1, this.salt)).hashedPassword,
 				passwordVersion: 1,
-				salt: 'G5wEYCwf2rFtgZq41j+yDYXAvgoGmKjwNWAM+wgG0f8iG3r3xRx9G+Inup5Gd0J521XMHXLQVIL/MJQa0YcOzw==',
+				salt: this.salt,
 				user: admin
 			})
 		);
 
 		/* Clubs */
 
-		const geekClub = new Club({ name: 'Geek Club', externalId: 1 });
-		await this.clubRepository.save(geekClub);
-
-		const fencingClub = new Club({ name: 'Fencing Club', externalId: 2 });
-		await this.clubRepository.save(fencingClub);
-
-		const bookClub = new Club({ name: 'Book Club', externalId: 3 });
-		await this.clubRepository.save(bookClub);
+		const groupsCreated = {} as { [key: string]: Club };
+		for (const g of this.groups) {
+			const obj = new Club({ name: g, externalId: 1 });
+			await this.clubRepository.save(obj);
+			groupsCreated[g] = obj;
+		}
 
 		/* Membershisp */
 
-		await this.membershipRepository.save(
-			new ClubMembership({
-				user: john,
-				club: geekClub,
-				clubRole: ClubRole.CLUB_MANAGER
-			})
-		);
-		await this.membershipRepository.save(
-			new ClubMembership({
-				user: john,
-				club: fencingClub
-			})
-		);
-		await this.membershipRepository.save(
-			new ClubMembership({
-				user: peter,
-				club: fencingClub
-			})
-		);
-		await this.membershipRepository.save(
-			new ClubMembership({
-				user: april,
-				club: bookClub,
-				clubRole: ClubRole.CLUB_MANAGER
-			})
-		);
-		await this.membershipRepository.save(
-			new ClubMembership({
-				user: emily,
-				club: bookClub
-			})
-		);
-		await this.membershipRepository.save(
-			new ClubMembership({
-				user: admin,
-				club: geekClub
-			})
-		);
-
-		/* Events */
-
-		const galaDinner = new Event({
-			// John already registered
-			name: 'Gala dinner',
-			uniqueName: 'gala-dinner',
-			description: 'Gala dinner',
-			place: 'SCH',
-			start: new Date(2020, 11, 23, 18, 0),
-			end: new Date(2020, 11, 23, 22, 58),
-			registrationStart: new Date(2020, 9, 20, 18, 0),
-			registrationEnd: new Date(2020, 11, 20, 12, 0),
-			isDateOrTime: false,
-			isClosedEvent: true,
-			hostingClubs: [geekClub, bookClub]
-		});
-		await this.eventRepository.save(galaDinner);
-
-		const semesterEndDinner = new Event({
-			// John can attend
-			name: 'Semester end dinner',
-			uniqueName: 'semester-end-dinner',
-			description: 'Semester end dinner',
-			place: 'SCH',
-			start: new Date(2020, 11, 24, 18, 0),
-			end: new Date(2020, 11, 24, 22, 58),
-			registrationStart: new Date(2020, 9, 20, 18, 0),
-			registrationEnd: new Date(2020, 11, 20, 12, 0),
-			isDateOrTime: false,
-			isClosedEvent: true,
-			hostingClubs: [geekClub]
-		});
-		await this.eventRepository.save(semesterEndDinner);
-
-		const privateParty = new Event({
-			// John can not attend
-			name: 'Private party',
-			uniqueName: 'private-party',
-			description: 'Private party',
-			place: 'SCH',
-			start: new Date(2020, 11, 24, 18, 0),
-			end: new Date(2020, 11, 24, 22, 58),
-			registrationStart: new Date(2020, 9, 20, 18, 0),
-			registrationEnd: new Date(2020, 11, 20, 12, 0),
-			isDateOrTime: false,
-			isClosedEvent: true,
-			hostingClubs: [bookClub]
-		});
-		await this.eventRepository.save(privateParty);
-
-		const birthdayParty = new Event({
-			// Public event, John can attend
-			name: 'Birthday party',
-			uniqueName: 'birthday-party',
-			description: 'Birthday party',
-			place: 'SCH',
-			start: new Date(2020, 11, 26, 18, 0),
-			end: new Date(2020, 11, 26, 22, 58),
-			registrationStart: new Date(2020, 9, 20, 18, 0),
-			registrationEnd: new Date(2020, 11, 20, 12, 0),
-			isDateOrTime: false,
-			isClosedEvent: false,
-			hostingClubs: [geekClub, bookClub]
-		});
-		await this.eventRepository.save(birthdayParty);
-
-		const johnsParty = new Event({
-			// John is chief organizer
-			name: "John' party",
-			uniqueName: 'johns-party',
-			description: "John' party",
-			place: 'SCH',
-			start: new Date(2020, 11, 26, 18, 0),
-			end: new Date(2020, 11, 26, 22, 58),
-			registrationStart: new Date(2020, 9, 20, 18, 0),
-			registrationEnd: new Date(2020, 11, 20, 12, 0),
-			isDateOrTime: false,
-			isClosedEvent: false,
-			hostingClubs: [geekClub]
-		});
-		await this.eventRepository.save(johnsParty);
-
-		const emilysParty = new Event({
-			// John is organizer
-			name: "Emily's party",
-			uniqueName: 'emily-party',
-			description: "Emily's party",
-			place: 'SCH',
-			start: new Date(2020, 11, 26, 18, 0),
-			end: new Date(2020, 11, 26, 22, 58),
-			registrationStart: new Date(2020, 9, 20, 18, 0),
-			registrationEnd: new Date(2020, 11, 20, 12, 0),
-			isDateOrTime: false,
-			isClosedEvent: false,
-			hostingClubs: [geekClub]
-		});
-		await this.eventRepository.save(emilysParty);
-
-		const birthday2020 = new Event({
-			// John is organizer
-			name: 'Birthday Party 2020',
-			uniqueName: 'bd-party2020',
-			description: 'Birthday Party 2020',
-			place: 'SCH',
-			start: new Date(2020, 5, 26, 18, 0),
-			end: new Date(2020, 5, 26, 22, 58),
-			registrationStart: new Date(2020, 4, 20, 18, 0),
-			registrationEnd: new Date(2020, 5, 20, 12, 0),
-			isDateOrTime: false,
-			isClosedEvent: false,
-			hostingClubs: [geekClub]
-		});
-		await this.eventRepository.save(birthday2020);
-
-		const birthday2019 = new Event({
-			// John is organizer
-			name: "Emily's party",
-			uniqueName: 'bd-party2019',
-			description: "Emily's party",
-			place: 'SCH',
-			start: new Date(2019, 5, 26, 18, 0),
-			end: new Date(2019, 5, 26, 22, 58),
-			registrationStart: new Date(2019, 4, 20, 18, 0),
-			registrationEnd: new Date(2019, 5, 20, 12, 0),
-			isDateOrTime: false,
-			isClosedEvent: false,
-			hostingClubs: [geekClub]
-		});
-		await this.eventRepository.save(birthday2019);
-
-		const aprilGalaDinner = new Organizer({
-			event: galaDinner,
-			user: april,
-			isChief: true,
-			notificationSettings: OrganizerNotificationSettings.ALL
-		});
-		await this.organizerRepository.save(aprilGalaDinner);
-
-		const emilyGalaDinner = new Organizer({
-			event: galaDinner,
-			user: emily,
-			isChief: false,
-			notificationSettings: OrganizerNotificationSettings.ALL
-		});
-		await this.organizerRepository.save(emilyGalaDinner);
-
-		const johnJohnParty = new Organizer({
-			event: johnsParty,
-			user: john,
-			isChief: true,
-			notificationSettings: OrganizerNotificationSettings.ALL
-		});
-		await this.organizerRepository.save(johnJohnParty);
-
-		const johnEmilyParty = new Organizer({
-			event: emilysParty,
-			user: john,
-			isChief: false,
-			notificationSettings: OrganizerNotificationSettings.ALL
-		});
-		await this.organizerRepository.save(johnEmilyParty);
-
-		const johnGalaDinner = new Registration({
-			event: galaDinner,
-			user: john,
-			registrationDate: new Date(),
-			notificationSettings: RegistrationNotificationSettings.ALL
-		});
-		await this.registrationRepository.save(johnGalaDinner);
-
-		const johnBd2020 = new Registration({
-			event: birthday2020,
-			user: john,
-			registrationDate: new Date('2020-05-01T00:00:00.000Z'),
-			notificationSettings: RegistrationNotificationSettings.ALL,
-			attendDate: new Date('2020-05-01T00:00:00.000Z')
-		});
-		await this.registrationRepository.save(johnBd2020);
-
-		const johnBd2019 = new Registration({
-			event: birthday2019,
-			user: john,
-			registrationDate: new Date('2019-05-01T00:00:00.000Z'),
-			notificationSettings: RegistrationNotificationSettings.ALL,
-			attendDate: new Date('2019-05-01T00:00:00.000Z')
-		});
-		await this.registrationRepository.save(johnBd2019);
-
-		/* Form */
-
-		const foodPreference = new FormQuestion({
-			question: 'Food preference',
-			event: galaDinner,
-			order: 0,
-			type: FormQuestionType.MULTIPLE_CHOICE,
-			typeMetadata: {
-				type: 'multiple_choice',
-				multipleAnswers: false,
-				options: [
-					{ id: 'regular', text: 'Regular' },
-					{ id: 'vegi', text: 'Vegetarian' }
-				]
-			},
-			isRequired: true
-		});
-
-		const nickName = new FormQuestion({
-			question: 'Nickname',
-			event: galaDinner,
-			order: 1,
-			type: FormQuestionType.TEXT,
-			typeMetadata: {
-				type: 'text',
-				maxLength: 25
-			},
-			isRequired: true
-		});
-
-		await this.formQuestionRepository.save([foodPreference, nickName]);
-
-		const nickName2 = new FormQuestion({
-			question: 'Nickname',
-			event: semesterEndDinner,
-			order: 1,
-			type: FormQuestionType.TEXT,
-			typeMetadata: {
-				type: 'text',
-				maxLength: 25
-			},
-			isRequired: true
-		});
-
-		await this.formQuestionRepository.save([nickName2]);
-
-		const nickName3 = new FormQuestion({
-			question: 'Nickname',
-			event: privateParty,
-			order: 1,
-			type: FormQuestionType.TEXT,
-			typeMetadata: {
-				type: 'text',
-				maxLength: 25
-			},
-			isRequired: true
-		});
-
-		await this.formQuestionRepository.save([nickName3]);
-
-		const johnFoodPreference = new FormQuestionAnswer({
-			formQuestion: foodPreference,
-			registration: johnGalaDinner,
-			answer: {
-				type: 'multiple_choice',
-				options: ['regular']
+		for (const [k, v] of Object.entries(this.memberships)) {
+			await this.membershipRepository.save(
+				new ClubMembership({
+					user: membersCreated[v.lead],
+					club: groupsCreated[k],
+					clubRole: ClubRole.CLUB_MANAGER
+				})
+			);
+			for (const m of v.members) {
+				await this.membershipRepository.save(
+					new ClubMembership({
+						user: membersCreated[m],
+						club: groupsCreated[k],
+						clubRole: ClubRole.MEMBER
+					})
+				);
 			}
-		});
-
-		const johnNickName = new FormQuestionAnswer({
-			formQuestion: nickName,
-			registration: johnGalaDinner,
-			answer: {
-				type: 'text',
-				text: 'Johnny'
-			}
-		});
-
-		await this.formAnswerRepository.save([johnFoodPreference, johnNickName]);
+		}
 
 		/* Template */
 
 		const foodPrefTemplate = new FormQuestionTemplate({
-			question: 'Food preference',
+			question: 'Étel érzékenység',
 			type: FormQuestionType.MULTIPLE_CHOICE,
 			typeMetadata: {
 				type: 'multiple_choice',
-				multipleAnswers: false,
+				multipleAnswers: true,
 				options: [
-					{ id: 'regular', text: 'Regular' },
-					{ id: 'vegi', text: 'Vegetarian' }
+					{ id: 'laktoz', text: 'Laktóz' },
+					{ id: 'gluten', text: 'Glutén' },
+					{ id: 'vega', text: 'Vegetáriánus' }
 				]
 			}
 		});
@@ -495,129 +230,5 @@ export class SeedService {
 		});
 
 		await this.formTempRepository.save([foodPrefTemplate, nickNameTemplate, accomodationTemplate]);
-
-		/* HR Table */
-
-		const galaHRTable = new HRTable({ event: galaDinner, isLocked: false });
-		await this.hrTableRepository.save(galaHRTable);
-
-		const setTablesTask = new HRTask({
-			name: 'Set table',
-			start: new Date(),
-			end: new Date(),
-			order: 0,
-			isLocked: false,
-			hrTable: galaHRTable
-		});
-
-		const waiteringTask = new HRTask({
-			name: 'Waitering',
-			start: new Date(),
-			end: new Date(),
-			order: 0,
-			isLocked: false,
-			hrTable: galaHRTable
-		});
-
-		await this.hrTaskRepository.save([setTablesTask, waiteringTask]);
-
-		const setTablesSegment = new HRSegment({
-			capacity: 1,
-			start: new Date(),
-			end: new Date(),
-			isRequired: true,
-			isLocked: false,
-			hrTask: setTablesTask,
-			organizers: [aprilGalaDinner]
-		});
-
-		const waiteringPrecourseSegment = new HRSegment({
-			capacity: 2,
-			start: new Date(),
-			end: new Date(),
-			isRequired: true,
-			isLocked: false,
-			hrTask: waiteringTask,
-			organizers: [aprilGalaDinner, emilyGalaDinner]
-		});
-
-		const waiteringMaincourseSegment = new HRSegment({
-			capacity: 1,
-			start: new Date(),
-			end: new Date(),
-			isRequired: false,
-			isLocked: false,
-			hrTask: waiteringTask,
-			organizers: []
-		});
-
-		await this.hrSegmentRepository.save([setTablesSegment, waiteringPrecourseSegment, waiteringMaincourseSegment]);
-
-		/* Postman environment */
-
-		const adminToken = (await this.authManager.loginWithLocalIdentity('admin', 'admin')).access;
-		const aprilToken = (await this.authManager.loginWithLocalIdentity('april', 'admin')).access;
-		const emilyToken = (await this.authManager.loginWithLocalIdentity('emily', 'admin')).access;
-		const johnToken = (await this.authManager.loginWithLocalIdentity('john', 'admin')).access;
-
-		/* eslint-disable max-len */
-		console.log(
-			JSON.stringify({
-				id: '55ec8ea2-4ac2-4f9f-aff3-84bd58cdd186',
-				name: 'Simonyi Rendezvény',
-				values: [
-					{
-						key: 'url',
-						value: 'http://localhost:3000/api/v1',
-						enabled: true
-					},
-					{
-						key: 'user_token',
-						value: emilyToken,
-						enabled: true
-					},
-					{
-						key: 'manager_token',
-						value: aprilToken,
-						enabled: true
-					},
-					{
-						key: 'admin_token',
-						value: adminToken,
-						enabled: true
-					},
-					{
-						key: 'registration_token',
-						value: await this.eventManager.getEventToken(
-							new AccessContext(this.jwtService.decode(johnToken) as AccessToken),
-							galaDinner
-						),
-						enabled: true
-					},
-					{
-						key: 'organizer_token',
-						value: await this.eventManager.getEventToken(
-							new AccessContext(this.jwtService.decode(emilyToken) as AccessToken),
-							galaDinner
-						),
-						enabled: true
-					},
-					{
-						key: 'chief_organizer_token',
-						value: await this.eventManager.getEventToken(
-							new AccessContext(this.jwtService.decode(aprilToken) as AccessToken),
-							galaDinner
-						),
-						enabled: true
-					},
-					{
-						key: 'event_id',
-						value: galaDinner.id,
-						enabled: true
-					}
-				]
-			})
-		);
-		/* eslint-enable max-len */
 	}
 }
