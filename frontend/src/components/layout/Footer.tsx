86c59bb58c0ccbbb6dbacc9323907d07d0e0b732
Freyb <balazs.frey@gmail.com>
2021-06-14 14:55:14 +0200

    Remove icon from Footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,25 +1,19 @@
-import { Box, Image } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import React from 'react';
-
-import logoSmall from '../../assets/images/simonyi_white_small.svg';
-import logo from '../../assets/images/simonyi_white_white.svg';
 
 export default function Footer(): JSX.Element {
   return (
-    <Box p={1} backgroundColor="simonyi">
-      <Image
-        display={['none', null, 'block']}
-        height="2rem"
-        src={logo}
-        alt="Logo"
-      />
-      <Image
-        display={['block', null, 'none']}
-        height="3rem"
-        m={1}
-        src={logoSmall}
-        alt="Logo"
-      />
+    <Box py={2} px={[4, null, 8]} backgroundColor="simonyi" textAlign="right">
+      <Box
+        as="a"
+        fontWeight="bold"
+        href="https://simonyi.bme.hu"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Simonyi Károly Szakkollégium
+      </Box>{' '}
+      - 2021
     </Box>
   );
 }
