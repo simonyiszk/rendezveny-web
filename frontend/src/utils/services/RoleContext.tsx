f53f1dd5656ed477f2a0d3d56faaf2bbb9a63de3
Freyb <balazs.frey@gmail.com>
2021-06-16 21:29:57 +0200

    Fix provider content loss on refresh

--- frontend/src/utils/services/RoleContext.tsx
+++ frontend/src/utils/services/RoleContext.tsx
@@ -38,13 +38,29 @@ export const RoleContext = createContext<ContextProps>({
   clearRelations: undefined,
 });
 
+const systemRoleToken = 'SIMONYI_RENDEZVENY_SYSTEM_ROLE';
+const eventRoleToken = 'SIMONYI_RENDEZVENY_EVENT_ROLE';
+
 interface Props {
   children: React.ReactNode;
 }
 
 export default function RoleProvider({ children }: Props): JSX.Element {
-  const [systemRole, setSystemRole] = useState(0);
-  const [eventRole, setEventRole] = useState(0);
+  const systemRoleFromStorage =
+    typeof window !== 'undefined'
+      ? localStorage.getItem(systemRoleToken)
+      : undefined;
+  const eventRoleFromStorage =
+    typeof window !== 'undefined'
+      ? localStorage.getItem(eventRoleToken)
+      : undefined;
+
+  const [systemRole, setSystemRole] = useState(
+    systemRoleFromStorage ? parseInt(systemRoleFromStorage, 10) : 0,
+  );
+  const [eventRole, setEventRole] = useState(
+    eventRoleFromStorage ? parseInt(eventRoleFromStorage, 10) : 0,
+  );
 
   const isMatch = (value: number, reference: number): boolean => {
     return (value & reference) === reference;
@@ -54,6 +70,10 @@ export default function RoleProvider({ children }: Props): JSX.Element {
     if (role === UserRole[UserRole.ADMIN]) relation |= SystemRoleTypes.ADMIN;
     if (memberships.some((m) => m.role === ClubRole[ClubRole.CLUB_MANAGER]))
       relation |= SystemRoleTypes.MANAGER;
+
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(systemRoleToken, relation.toString());
+    }
     setSystemRole(relation);
   };
   const setEventRelation = (data: EventTokenType): void => {
@@ -68,9 +88,17 @@ export default function RoleProvider({ children }: Props): JSX.Element {
       relation |= EventRoleTypes.HOSTING_MEMBER;
     if (data.events_getToken.relation.isManagerOfHostingClub)
       relation |= EventRoleTypes.HOSTING_MANAGER;
+
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(eventRoleToken, relation.toString());
+    }
     setEventRole(relation);
   };
   const clearRelations = () => {
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem(systemRoleToken);
+      localStorage.removeItem(eventRoleToken);
+    }
     setSystemRole(0);
     setEventRole(0);
   };
