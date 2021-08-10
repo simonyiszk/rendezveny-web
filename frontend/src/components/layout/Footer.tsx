a33616ceee960755890d3b7bdf688a2c8c56742f
Freyb <balazs.frey@gmail.com>
2021-08-10 14:06:41 +0200

    Add app version to footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,19 +1,37 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import React from 'react';
 
 export default function Footer(): JSX.Element {
   return (
-    <Box py={2} px={[4, null, 8]} backgroundColor="simonyi" textAlign="right">
+    <Flex
+      py={2}
+      px={[4, null, 8]}
+      backgroundColor="simonyi"
+      justifyContent="space-between"
+      flexDirection={['column', null, 'row']}
+      alignItems="center"
+    >
+      <Box textAlign="center">
+        <Box
+          as="a"
+          fontWeight="bold"
+          href="https://simonyi.bme.hu"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Simonyi Károly Szakkollégium
+        </Box>{' '}
+        - 2021
+      </Box>
       <Box
         as="a"
         fontWeight="bold"
-        href="https://simonyi.bme.hu"
+        href="https://github.com/simonyiszk/rendezveny-web"
         target="_blank"
         rel="noreferrer"
       >
-        Simonyi Károly Szakkollégium
-      </Box>{' '}
-      - 2021
-    </Box>
+        v0.2.2-beta
+      </Box>
+    </Flex>
   );
 }
